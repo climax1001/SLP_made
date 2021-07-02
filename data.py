a5fc290abf013752611d3742b4ca2db99cdd5ca5
juncislab <climax1001>
2021-07-02 18:14:46 +0900

    6/30 마무리 7/1 별거없 7/2 영어->한글어번역 예제 transformer

--- data.py
+++ data.py
@@ -1,18 +1,139 @@
-import nltk
-from konlpy.tag import Okt
-from konlpy.tag import Kkma
-from Korpora import Korpora
-okt = Okt()
-kkm = Kkma()
-text_set = ['나는 밥을 먹는 것을 좋아합니다.',
-            '동물원에 사자가 있습니다.',
-            '집에 불이 났습니다.']
+from random import random
 
-# for i, line in enumerate(text_set):
-#     set_pos = kkm.pos(line)
-#     print(set_pos)
-#     set_noun = kkm.nouns(line)
-#     print(set_noun)
+# import nltk
+# import numpy as np
+# from konlpy.tag import Oktrom konlpy.tag import Kkma
+# from Korpora import Korpora
 
-corpus = Korpora.load("modu_spoken")
+import unicodedata
+import torch
+import os
+import torch.nn as nn
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+import re
+SOS_token = 0
+EOS_token = 1
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0 : "SOS", 1: "EOS"}
+        self.n_words = 2
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# 소문자, 다듬기, 그리고 문자가 아닌 문자 제거
+
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    # s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # 파일을 읽고 줄로 분리
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # 모든 줄을 쌍으로 분리하고 정규화
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+
+    # 쌍을 뒤집고, Lang 인스턴스 생성
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+
+def filterPair(p):
+    return len(p[1].split(' ')) < MAX_LENGTH and \
+        len(p[0].split(' ')) < MAX_LENGTH and \
+        p[0].startswith(eng_prefixes)
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+def prepareData(reverse=False):
+    input_lang, output_lang, pairs = load_data(reverse)
+    print("Read %s sentence pairs" % len(pairs))
+
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+# lines = open('data/%s-%s.txt' % ('eng', 'kor')).\
+#         read().strip().split('\n')
+# print(lines)
+# pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+# print(len(pairs))
+def load_data(reverse=False):
+    lines = open('data/eng-kor.txt', encoding='utf-8').\
+        read().strip().split('\n')
+    word = []
+
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang('kor')
+        output_lang = Lang('eng')
+    else:
+        input_lang = Lang('eng')
+        output_lang = Lang('kor')
+    for pair in pairs:
+        if len(pair) == 3:
+            word.append([pair[1], pair[0]])
+
+
+    return input_lang, output_lang, word
+
+i_l, o_l, pairs = prepareData()
+
+print(i_l.word2index)
+print(o_l.word2index)
+
+# print(len(list))
 
